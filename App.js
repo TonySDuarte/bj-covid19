b97a546844a25993e161eadcc86a755d982bbff4
TonySDuarte <tonysduarte101@gmail.com>
2021-05-23 21:41:06 -0300

    save

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import Boletim from './src/pages/Boletim/index.js';
+import Vacinometro from './src/pages/Vacinometro/index.js';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    // <Boletim />
+    <Vacinometro />
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
